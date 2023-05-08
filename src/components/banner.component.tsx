de8eacefc941a6627c8722e151affaabd4e35ca2
Britta <brittan.villegas@gmail.com>
2023-05-08 14:55:27 +0200

    added framer motion animation to banner title elements

--- src/components/banner.component.tsx
+++ src/components/banner.component.tsx
@@ -1,7 +1,7 @@
 import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
 import { TypeAnimation } from 'react-type-animation';
-// import { motion } from 'framer-motion';
-// import { fadeIn } from '../variants';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
 import BannerPic from '../assets/banner-pic.png'
 
 const Banner = () => {
@@ -10,10 +10,20 @@ const Banner = () => {
             <div className='flex flex-col justify-around gap-y-6 lg:flex-row lg:items-center text-black/70'>
                 <div className='text-center lg:text-left'>
                     <div className='pt-6 pb-3'>
-                        <h1 className='text-[2rem] font-bold uppercase leading-[0.8] mb-4 lg:text-[3rem]'>
+                        <motion.h1
+                            variants={fadeIn('up', 0.2)}
+                            initial='hidden'
+                            whileInView={'show'}
+                            viewport={{ once: false, amount: 0.7 }}
+                            className='text-[2rem] font-bold uppercase leading-[0.8] mb-4 lg:text-[3rem]'>
                             Brittany <span>Villegas</span>
-                        </h1>
-                        <div className='text-[1.5rem] lg:text-[2.5rem] font-secondary font-semibold uppercase leading-[1]'>
+                        </motion.h1>
+                        <motion.div
+                            variants={fadeIn('up', 0.2)}
+                            initial='hidden'
+                            whileInView={'show'}
+                            viewport={{ once: false, amount: 0.7 }}
+                            className='text-[1.5rem] lg:text-[2.5rem] font-secondary font-semibold uppercase leading-[1]'>
                             <span className='text-white'>I am a </span>
                             <TypeAnimation sequence={[
                                 'developer.',
@@ -33,7 +43,7 @@ const Banner = () => {
                                 className=''
                                 wrapper='span'
                                 repeat={Infinity} />
-                        </div>
+                        </motion.div>
                     </div>
                     <div className='max-w-lg mx-auto lg:mx-0 font-tertiary py-3'>
                         <p>I'm a web developer who is passionate about problem solving, interpersonal communications, strategic thinking, leading teams, empowering others, and building user-centric apps and services. </p>
