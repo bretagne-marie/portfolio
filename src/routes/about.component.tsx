1467d2db4aa3546480d8130b6ca71d6f9d2569a7
Britta <brittan.villegas@gmail.com>
2023-05-09 13:54:50 +0200

    Update about.component.tsx

--- src/routes/about.component.tsx
+++ src/routes/about.component.tsx
@@ -1,14 +1,14 @@
-import CountUp from 'react-countup';
-import { useInView } from 'react-intersection-observer'
-import { motion } from 'framer-motion'
-import { fadeIn } from '../variants';
+// import CountUp from 'react-countup';
+// import { useInView } from 'react-intersection-observer'
+// import { motion } from 'framer-motion'
+// import { fadeIn } from '../variants';
 
 const About = () => {
-    const [ref, inView ] = useInView({
-        threshold: 0.5,
-    });
+    // const [ref, inView ] = useInView({
+    //     threshold: 0.5,
+    // });
     return (
-        <section className="section h-screen" ref={ref}>
+        <section className="section h-screen">
             <div className="container mx-auto">
                 <h1>This is the about section</h1>
             </div>
