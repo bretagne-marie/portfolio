effd3de2323750119c909f0be110ca1549b7ba04
Britta <brittan.villegas@gmail.com>
2023-05-10 23:57:01 +0200

    added types and updated about route

---
+++ src/types/Types.tsx
@@ -0,0 +1,8 @@
+export type ICardProps = {
+    title: string;
+    subtitle?: string | null;
+    text?: string | null;
+    image: string;
+    btnLink?: string | null;
+    aLink?: string | null;
+}
